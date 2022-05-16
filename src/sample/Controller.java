bbf27520d3e0f8fc9350331cc649da1bf1f4784d
Florian Lanz <florianlanz73@gmail.com>
2022-05-16 09:30:54 +0200

    fixed bug of 2 same exercises in a row

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,6 +14,8 @@ public class Controller {
     private final int rows = 3;
     private final int offsetX = lineLength / (cols-1);
     private final int offsetY = lineLength / (rows-1);
+    private int oldX = 0;
+    private int oldY = 0;
 
     @FXML
     private Pane click_field;
@@ -30,17 +32,24 @@ public class Controller {
     @FXML
     void btnClicked(MouseEvent event) {
         Random random = new Random();
-        int rndHor = random.nextInt(cols);
-        int rndVer = random.nextInt(rows);
+
         if (random.nextFloat() > 0.7f) {
             click_field.setLayoutX(random.nextInt((int) test_area.getWidth()-100));
             click_field.setLayoutY(random.nextInt((int) test_area.getHeight()-100));
         }
 
-        hori_line.setStartY(rndVer*offsetY);
-        hori_line.setEndY(rndVer*offsetY);
-        vert_line.setStartX(rndHor*offsetX);
-        vert_line.setEndX(rndHor*offsetX);
+        do {
+            int rndHor = random.nextInt(cols);
+            int rndVer = random.nextInt(rows);
+            hori_line.setStartY(rndVer * offsetY);
+            hori_line.setEndY(rndVer * offsetY);
+            vert_line.setStartX(rndHor * offsetX);
+            vert_line.setEndX(rndHor * offsetX);
+        } while ((int) hori_line.getStartY() == oldY && (int) vert_line.getStartX() == oldX);
+
+        oldY = (int) hori_line.getStartY();
+        oldX = (int) vert_line.getStartX();
+
 
     }
 
