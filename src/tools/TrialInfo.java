c0b78cc861c116b019db4e35030223983fadea01
Florian Lanz <florianlanz73@gmail.com>
2022-06-13 12:27:10 +0200

    added csv

--- src/tools/TrialInfo.java
+++ src/tools/TrialInfo.java
@@ -3,7 +3,7 @@ package tools;
 public class TrialInfo {
     public static class INFO {
         public final static String PARTICIPANT_ID = "1";
-        public final static String CSV_FILE_NAME = "participant" + INFO.PARTICIPANT_ID + ".csv";
+        public final static String CSV_FILE_NAME = "csv/participant" + INFO.PARTICIPANT_ID + ".csv";
         public final static String BLOCK_NR = "1";
         public final static String TRIAL_NR = "1";
         public final static String TASK_ID = "1";
